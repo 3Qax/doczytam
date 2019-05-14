cfaec7ff86f195bc7e88b7c17e2715a96b11b684
Jakub Towarek <kuba@towarek.pl>
2019-05-14 02:06:04 +0200

    Setup project
    Add SwiftLint with default configuration and SnapKit

---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '12.0'
+
+target 'doczytam' do
+  use_frameworks!
+
+  # Pods for doczytam
+	pod 'SwiftLint', '0.32.0'
+	pod 'SnapKit', '5.0.0'
+
+end
---
+++ doczytam/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  doczytam
+//
+//  Created by Jakub Towarek on 14/05/2019.
+//  Copyright © 2019 Jakub Towarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
